3a0cec4aa853f84929677eaac90637d506570c00
Stijn van der Kolk <stijn@kolkies.dev>
2021-11-18 10:26:41 +0000

    fix: move app module to seperate folder
    Co-authored-by: DeBobbel <emilykok@users.noreply.github.com>

---
+++ backend/src/modules/App/app.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  UseGuards,
+  Request,
+  Post,
+  Param,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { AppService } from './app.service';
+
+@Controller('/')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/teapot') // Test http code, add '/' to controler methods for conventional reasons!
+  @HttpCode(418)
+  getTeapot(): string {
+    return 'Do you have some time for tea?';
+  }
+
+  @Get('/user/create')
+  async createUser(): Promise<User> {
+    return this.appService.createUser();
+  }
+
+  @Get('/user/:email')
+  async getUser(@Param('email') email: string): Promise<User> {
+    return this.appService.getUser(email);
+  }
+}
--- backend/src/modules/App/app.module.ts
+++ backend/src/modules/App/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../Auth/auth.module';
+import { PrismaModule } from '../Prisma/prima.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
