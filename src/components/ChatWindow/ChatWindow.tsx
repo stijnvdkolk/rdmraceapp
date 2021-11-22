1ed4b817a6c10b25ba7f231bcc1af44994318af7
HugoVG <64896329+HugoVG@users.noreply.github.com>
2021-11-22 11:52:29 +0100

    Henk

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,11 +1,3 @@
-import { useParams } from "react-router-dom";
-
-export default function ChatWindow() {
-    
-    let {channelID} = useParams<{ channelID?: string }>();
-    return (
-        <div id="Windows">
-            <p>{channelID}</p>
-        </div>
-    );    
+export default function henk(){
+    return "";
 }
