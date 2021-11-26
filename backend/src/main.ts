c92f554964d6932700bc487f7b7cedd3281b8e49
Stijn van der Kolk <stijn@kolkies.dev>
2021-11-26 10:40:34 +0000

    feat(backend): enable cors and express user typing
    Co-authored-by: DeBobbel <emilykok@users.noreply.github.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/App/app.module';
+import { User } from '@prisma/client';
+
+declare module 'express' {
+  interface Request {
+    user: User;
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
