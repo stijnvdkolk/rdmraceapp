b01afbb04e6e11912595560eaffea361e7419acf
Stijn van der Kolk <stijn@kolkies.dev>
2021-11-26 10:40:56 +0000

    feat(backend/modules): auth hell
    Co-authored-by: DeBobbel <emilykok@users.noreply.github.com>

--- backend/src/modules/Auth/auth.module.ts
+++ backend/src/modules/Auth/auth.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../User/user.module';
-import { JwtStrategy } from './jwt.strategy';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [
-    UserModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: 18000 },
-    }),
-  ],
-  providers: [AuthService, JwtStrategy],
-  exports: [JwtStrategy],
+  imports: [UserModule],
+  providers: [AuthService],
+  exports: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
