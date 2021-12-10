d116a75bc3e86da5fbccf9f2020c86c12053b725
Stijn van der Kolk <stijn@kolkies.dev>
2021-12-10 13:16:45 +0000

    feat(modules/channel): channel service progress
    Co-authored-by: DeBobbel <emilykok@users.noreply.github.com>

--- backend/src/modules/Channel/channel.service.ts
+++ backend/src/modules/Channel/channel.service.ts
@@ -1,19 +1,11 @@
 import { NotFoundError } from '@lib/errors';
 import { Injectable } from '@nestjs/common';
-import { Channel, User } from '@prisma/client';
+import { Channel, ChannelType, User, UserRole } from '@prisma/client';
 import { PrismaService } from '../Prisma/prisma.service';
 
 @Injectable()
 export class ChannelService {
   constructor(private prisma: PrismaService) {}
-
-  // async findUserById(userId: User['id']): Promise<User> {
-  //   try {
-  //     return this.prisma.user.findUnique({ where: { id: userId } });
-  //   } catch (error) {
-  //     throw new NotFoundError('user_not_found');
-  //   }
-  // }
 
   async findChannelById(channelId: Channel['id']): Promise<Channel> {
     try {
@@ -23,48 +15,54 @@ export class ChannelService {
     }
   }
 
-  async findChannelByName(channelName: Channel['name']): Promise<Channel> {
-    try {
-      return this.prisma.channel.findFirst({ where: { name: channelName } });
-    } catch (error) {
-      throw new NotFoundError('channel_not_found');
-    }
+  async getDirectMessageChannelsOfUser(userId: User['id']): Promise<Channel[]> {
+    return this.prisma.channel.findMany({
+      where: {
+        users: {
+          some: {
+            id: userId,
+          },
+        },
+        type: ChannelType.DM,
+      },
+    });
   }
 
-  async findChannelByUserId(userId: User['id']): Promise<Channel[]> {
+  async getChannels(): Promise<Channel[]> {
     try {
       return this.prisma.channel.findMany({
-        where: { users: { some: { id: userId } } },
+        where: {
+          type: {
+            in: [
+              ChannelType.NEWS_CHANNEL,
+              ChannelType.PUBLIC_CHANNEL,
+              ChannelType.PRIVATE_CHANNEL,
+            ],
+          },
+        },
       });
     } catch (error) {
       throw new NotFoundError('channel_not_found');
     }
   }
 
-  async createChannel(channel: Channel): Promise<Channel> {
-    try {
-      return this.prisma.channel.create({ data: channel });
-    } catch (error) {
-      throw new NotFoundError('channnel_already_exists');
-    }
-  }
-
-  // async updateChannel(channelUpd: Channel): Promise<Channel> {
-  //   try {
-  //     return this.prisma.channel.update({
-  //       where: { id: channelUpd['id'] },
-  //       data: { channelUpd },
-  //     });
-  //   } catch (error) {
-  //     throw new NotFoundError('channel_not_found');
-  //   }
-  // }
-
-  async deleteChannel(channelId: Channel['id']): Promise<Channel> {
-    try {
-      return this.prisma.channel.delete({ where: { id: channelId } });
-    } catch (error) {
-      throw new NotFoundError('channel_not_found');
-    }
+  async createDMChannel(creator: User, otherUser: User): Promise<Channel> {
+    const channel = await this.prisma.channel.create({
+      data: {
+        name: 'DM Channel',
+        users: {
+          connect: [
+            {
+              id: creator.id,
+            },
+            {
+              id: otherUser.id,
+            },
+          ],
+        },
+        type: ChannelType.DM,
+      },
+    });
+    return channel;
   }
 }
