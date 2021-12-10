d8f4d2f71200e489bc67461f8d595b79c3919e10
Hogo <64896329+HugoVG@users.noreply.github.com>
2021-12-10 16:56:37 +0100

    Chat Messages Goed ; Wie test of het werkt? <Images namen, plaatjes huidig alleen te verkrijgen via "Hugo's Test API"> <Zelf getest op darkmode, light mode en opverschillende devices>

--- frontend/src/components/ChatWindow/ChatWindow.tsx
+++ frontend/src/components/ChatWindow/ChatWindow.tsx
@@ -1,15 +1,16 @@
-import { Card, Divider, TextField } from "@mui/material";
+import { Avatar, Card, Divider, TextField } from "@mui/material";
+import { GridRow } from "@mui/x-data-grid";
 import * as react from 'react';
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getMessage, getMessages } from "../../API/Chat";
 import Message, { Messages } from "../../classes/Message";
 //import { useParams } from "react-router-dom";
 import IProps from "../IProps";
+import { ThemeContext } from "../theme-context";
 import "./ChatWindow.css";
 
 function FkDateTime(date: Date) {
     var dtm: Date	= new Date(date);
-    console.log(dtm);
 
     var retVal: string = `${dtm.getDate()}/${dtm.getMonth() + 1}/${dtm.getFullYear()} ${dtm.getHours()}:${dtm.getMinutes()}:${dtm.getSeconds()}`;
 
@@ -18,6 +19,7 @@ function FkDateTime(date: Date) {
 }
 
 export default function ChatWindow(props: IProps){
+    const { colorTheme } =  useContext(ThemeContext);
     const { name, children, imageLink } = props;
     //let  {channelID}  = useParams<{channelID : string}>();
     const [matches, setMatches] = react.useState(window.matchMedia("(min-width: 1000px)").matches);
@@ -29,7 +31,7 @@ export default function ChatWindow(props: IProps){
     const [isMessageLoaded, setIsMessageLoaded] = useState<boolean>(false);
     const [messages, setMessages] = useState<Message[] | undefined>(undefined); //Message
     useEffect(() => {       
-        getMessages(2)
+        getMessages(10)
         .then(
             (result) => {
                 setIsMessageLoaded(true);
@@ -42,20 +44,37 @@ export default function ChatWindow(props: IProps){
         )
         
     }, [name]);
+    console.log(messages);
     const conversation = (
         <div className="conversation">
             {messages && messages.map((berichten, index) => (
                 
-                <div key={index}>
-                    <div className="message">
+                <div key={index} className={colorTheme  === "dark" ? "messageParent darkmessage" : "messageParent lightmessage"} >
+                    <div className={"message"}>  {/* TODO: Maak er een tabel van */}
                         <div className="message-text">
                             {berichten.content}
                         </div>
-                        <div className="message-time">
-                            { FkDateTime(berichten.createdAt as Date) } 
+                        <div className="message-info">
+                            <div className="message-time">
+                                { FkDateTime(berichten.createdAt as Date) } 
+                            </div>
+                            <div className="message-sender">
+                                {berichten?.author?.name}
+                            </div>
                         </div>
+                        <div className="message-image">
+                            <Avatar src={berichten?.author?.profilePicture} alt="" sx={{
+                                width: "50px",
+                                height: "50px",
+                            }}
+                                />
+                        </div>
+                        {berichten.attachments && berichten.attachments.map((attachment, index) => (
+                            <div key={index} className={`message-image-content`} style={{gridRow: index+4}}>
+                                <img src={attachment.toString()} width={200} alt="" />
+                            </div>
+                        ))}
                     </div>
-                    <Divider />
                 </div>
             ))}
             {/* <div className="message">
@@ -85,6 +104,9 @@ export default function ChatWindow(props: IProps){
         <div className="sendForm">
             <div className="sendFormContainer">
                 <div className="sendFormInner">
+                    <Divider sx={{
+                        marginBottom: "10px",
+                    }}/>
                     <TextField id="outlined-basic" label="Outlined" variant="outlined" className="MessageInput"/>
                 </div>
             </div>
@@ -97,9 +119,12 @@ export default function ChatWindow(props: IProps){
                     width: matches ? "95%" : "90%",
                     height: matches ? "90vh" : "100%",
                     borderRadius: "16px",
-                }}>
+                    display: "grid",
+                    gridTemplateColumns: "auto",
+                    gridTemplateRows: "72px 2px auto 10vh",
+                }} >
                 {chatHeaderContainer}
-                <Divider/>                
+                <Divider style={{gridRow: 2}} />                
                 {conversation /* this is where the chat messages go */}
                 {SendForm /*this is the send input*/}
             </Card>
