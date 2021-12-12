da590f667ef187f5be9878ed7ce35157a0a3cf2a
Stijn van der Kolk <stijn@kolkies.dev>
2021-12-12 16:27:04 +0000

    feat(modules/App): cleanup
    Co-authored-by: DeBobbel <emilykok@users.noreply.github.com>

--- backend/src/modules/App/app.controller.ts
+++ backend/src/modules/App/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { Public } from '@decorators';
-import { AppService } from './app.service';
 
 @Controller('/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('/teapot') // Test http code, add '/' to controler methods for conventional reasons!
   @Public()
   @HttpCode(418)
