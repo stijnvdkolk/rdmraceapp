a9e29da31253e85e2dc7ff494fc48099a4e8109b
DavidGiesenbot <1015843@hr.nl>
2022-01-03 15:13:32 +0100

    login notification
    notification when invalid login details

--- frontend/src/API/ApiCalls.ts
+++ frontend/src/API/ApiCalls.ts
@@ -1,93 +1,99 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export async function get(url: string) {
-    return fetch(baseUrl + url, {
-        method: "GET",
-    }).then(parseJson).then((response) => {
-        if (response.ok) {
-            return response.json;
-
-        }
-        throw new Error("message");
+  return fetch(baseUrl + url, {
+    method: "GET",
+  })
+    .then(parseJson)
+    .then((response) => {
+      if (response.ok) {
+        return response.json;
+      }
+      throw new Error("message");
     });
 }
 export async function getJWT(url: string) {
-    return fetch(baseUrl + url, {
-        method: "GET",
-        headers: {
-            "content-type": "application/json",
-            "authorization": `${localStorage.getItem("DogeToken")}`,
-        },
-    }).then(parseJson).then((response) => {
-        if (response.ok) {
-            return response.json;
-        } else if (response.status === 401) {
-            localStorage.removeItem("DogeToken");
-            window.location.href = "/Login";
-            return;
-        }
-        throw new Error("bruh");
+  return fetch(baseUrl + url, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      authorization: `${localStorage.getItem("DogeToken")}`,
+    },
+  })
+    .then(parseJson)
+    .then((response) => {
+      if (response.ok) {
+        return response.json;
+      } else if (response.status === 401) {
+        localStorage.removeItem("DogeToken");
+        window.location.href = "/Login";
+        return;
+      }
+      throw new Error("bruh");
     });
 }
 
-
-
-
 export async function postJson(url: string, data: any) {
-    return fetch(baseUrl + url, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify(data),
-    }).then(parseJson).then((response) => {
-        if (response.ok) {
-            return response.json;
-        }
+  return fetch(baseUrl + url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(parseJson)
+    .then((response) => {
+      if (response.ok) {
+        return response.json;
+      } else if (response.status === 401) {
+        return response.json;
+      }
     });
 }
 export async function postTokenJson(url: string, data: any) {
-    return fetch(baseUrl + url, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify(data),
-        }).then(response => response.json())
-        .then((response) => {
-            return response;
-        });
+  return fetch(baseUrl + url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      return response;
+    });
 }
 
 async function parseJson(response: Response) {
-    const contentType = response.headers.get("content-type");
-    if (contentType && contentType.indexOf("application/json") !== -1) {
-        return response.json()
-            .then((json: any) => ({
-                status: response.status,
-                ok: response.ok,
-                json,
-            }));
-    }
+  const contentType = response.headers.get("content-type");
+  if (contentType && contentType.indexOf("application/json") !== -1) {
+    return response.json().then((json: any) => ({
+      status: response.status,
+      ok: response.ok,
+      json,
+    }));
+  }
 
-    return {
-        status: response.status,
-        ok: response.ok,
-        json: {},
-    };
+  return {
+    status: response.status,
+    ok: response.ok,
+    json: {},
+  };
 }
-///This Consumes an effect so there won't be 500 lines of code in Pages 
-export function ConsumeEffect(loader: React.Dispatch < React.SetStateAction < boolean >> ,
-    setter: React.Dispatch < React.SetStateAction < any >> ,
-    callback: () => Promise < any > ) {
-    callback().then(
-        (result) => {
-            setter(result);
-            loader(true);
-        },
-        (error) => {
-            setter(error);
-            loader(false);
-        }
-    );
+///This Consumes an effect so there won't be 500 lines of code in Pages
+export function ConsumeEffect(
+  loader: React.Dispatch<React.SetStateAction<boolean>>,
+  setter: React.Dispatch<React.SetStateAction<any>>,
+  callback: () => Promise<any>
+) {
+  callback().then(
+    (result) => {
+      setter(result);
+      loader(true);
+    },
+    (error) => {
+      setter(error);
+      loader(false);
+    }
+  );
 }
--- frontend/src/pages/Debug/Debug.tsx
+++ frontend/src/pages/Debug/Debug.tsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
-import '../../App.css';
+import "../../App.css";
 import Notification from "../../components/notification/notification";
+import Button from "@mui/material/Button";
 export default function Debug() {
   return (
-
     <>
-        <div className="vh">
+      <div className="vh">
         <Link to="/Login">Login</Link>
         <Link to="/SignUp">Signup</Link>
         <Link to="/Invalid">Invalid</Link>
         <Link to="/Chat">Chat</Link>
         <Link to="/Chat/Publiek">Chat/Publiek</Link>
         <Link to="/Admin">Admin</Link>
-        <Notification message="Invalid email and/or password" notifyType={1}/>
+        <Notification message="Invalid email and/or password" />
       </div>
     </>
-    );
+  );
 }
