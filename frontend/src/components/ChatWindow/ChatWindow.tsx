8e23c3e0fe2546b17b4e651219ed611ac5a37825
Hogo <64896329+HugoVG@users.noreply.github.com>
2022-01-04 16:49:41 +0100

    Quick Commit
    Morgen nog verder hier aan

--- frontend/src/components/ChatWindow/ChatWindow.tsx
+++ frontend/src/components/ChatWindow/ChatWindow.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, Divider, FormControl, InputAdornment, TextField } from "@mui/material";
+import { Avatar, Card, Divider, FormControl, InputAdornment, Menu, MenuItem, TextField } from "@mui/material";
 import * as react from 'react';
 import { useContext, useEffect, useState } from "react";
 import { getMessages } from "../../API/Chat";
@@ -12,6 +12,9 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import "./ChatWindow.css";
 import { ConsumeEffect } from "../../API/ApiCalls";
 import { useParams } from "react-router-dom";
+import { ChildCare, SaveAltOutlined, ThumbUp, ThumbUpOutlined } from "@mui/icons-material";
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+
 
 function FkDateTime(date: Date) {
     var dtm: Date	= new Date(date);
@@ -33,6 +36,91 @@ export default function ChatWindow(props: IProps){
         window.matchMedia("(min-width: 1000px)").addListener(handler);
     }, [setMatches]);
     //#endregion
+
+    //#region Context Menu Bad Code
+    //TODO Refactor
+    const [contextMenu, setContextMenu] = react.useState<{
+        mouseX: number;
+        mouseY: number;
+    } | null>(null);
+    
+      const handleContextMenu = (event: React.MouseEvent) => {
+        event.preventDefault();
+        setContextMenu(
+          contextMenu === null
+            ? {
+                mouseX: event.clientX - 2,
+                mouseY: event.clientY - 4,
+              }
+            : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+              // Other native context menus might behave different.
+              // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+              null,
+        );
+      };
+
+    const handleClose = () => {
+        setContextMenu(null);
+    };
+
+
+    const [contextMenu2, setContextMenu2] = react.useState<{
+        mouseX: number;
+        mouseY: number;
+      } | null>(null);
+    
+      const handleContextMenu2 = (event: React.MouseEvent) => {
+        event.preventDefault();
+        
+        setContextMenu2(
+          contextMenu2 === null
+            ? {
+                mouseX: event.clientX - 2,
+                mouseY: event.clientY - 4,
+              }
+            : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+              // Other native context menus might behave different.
+              // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+              null,
+        );
+        
+      };
+
+    const handleClose2 = () => {
+        
+        setContextMenu2(null);
+    };
+
+    //TODO: REFACTOR THIS CODE
+    //TODO: Voeg PFP Click
+    const [contextMenu3, setContextMenu3] = react.useState<{
+        mouseX: number;
+        mouseY: number;
+      } | null>(null);
+    
+      const handleContextMenu3 = (event: React.MouseEvent) => {
+        event.preventDefault();
+        
+        setContextMenu3(
+          contextMenu3 === null
+            ? {
+                mouseX: event.clientX - 2,
+                mouseY: event.clientY - 4,
+              }
+            : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+              // Other native context menus might behave different.
+              // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+              null,
+        );
+        
+      };
+
+    const handleClose3 = () => {
+        
+        setContextMenu3(null);
+    };
+    //#endregion
+
     //#region MessageLoader
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [error, setError] = react.useState(false);
@@ -46,15 +134,22 @@ export default function ChatWindow(props: IProps){
     //#region Message
     const conversation = (
         <div className="conversation">
-            {messages !== undefined && messages.length !== 0 && (
-                 messages.map((berichten, index) => (
-                
-                <div key={index} className={colorTheme  === "dark" ? "messageParent darkmessage" : "messageParent lightmessage"} >
+            {messages !== undefined && messages.length !== 0 ? (
+                messages.map((berichten, index) => ( 
+                    <>               
+                <div key={index} 
+                    className={colorTheme  === "dark" ? "messageParent darkmessage" : "messageParent lightmessage"} 
+                    >                    
+
                     <div className={"message"}> 
-                        <div className="message-text">
+                        <div className="message-text" 
+                            onContextMenu={handleContextMenu} 
+                            style={{cursor: "context-menu"}}>
                             {berichten.content}
                         </div>
-                        <div className="message-info">
+                        <div className="message-info"
+                           onContextMenu={handleContextMenu} 
+                           style={{cursor: "context-menu"}}>
                             <div className="message-time">
                                 { FkDateTime(berichten.createdAt as Date) } 
                             </div>
@@ -70,13 +165,86 @@ export default function ChatWindow(props: IProps){
                                 />
                         </div>
                         {berichten.attachments && berichten.attachments.map((attachment, index) => (
-                            <div key={index} className={`message-image-content`} style={{gridRow: index+4}}>
-                                <img src={attachment.toString()} width={200} alt="" className="Picture" />
-                            </div>
+                                <div key={index} className={`message-image-content`} 
+                                    style={{gridRow: index+4, cursor: "context-menu"}}
+                                    onContextMenu={handleContextMenu2}>
+                                    <img src={`https://cdn.rdmraceapp.nl/attachments/${channelID}/${berichten.id}/${attachment.name}`} width={200} alt="" className="Picture" />
+                                    <Menu
+                                        open={contextMenu2 !== null}
+                                        onClose={handleClose2}
+                                        anchorReference="anchorPosition"
+                                        anchorPosition={
+                                            contextMenu2 !== null ?
+                                            {top: contextMenu2!.mouseY, left: contextMenu2!.mouseX} :
+                                            undefined
+                                        }>
+                                        <MenuItem onClick={handleClose2}>
+                                            <div className="MenuItemWithIcon">
+                                                <AttachFileIcon/>
+                                                {"Copy Image"}
+                                            </div>                                            
+                                        </MenuItem>
+                                        <MenuItem onClick={handleClose2}>
+                                            <div className="MenuItemWithIcon">
+                                                <SaveAltOutlined/>
+                                                {"Save Image"}
+                                            </div>                                            
+                                        </MenuItem>
+                                        <MenuItem onClick={handleClose}> {/*TODO: Copy to clipboard*/}
+                                            <div className="MenuItemWithIcon">
+                                                <PersonOutlinedIcon/>
+                                                {"Author Profile"}
+                                            </div>                                            
+                                        </MenuItem>
+                                        <MenuItem onClick={handleClose}> 
+                                            <div className="MenuItemWithIcon">
+                                                <ThumbUpOutlined/>
+                                                {"Nice Embed Bro"}
+                                            </div>                                            
+                                        </MenuItem>
+                                    </Menu>
+                                </div>
                         ))}
+                        
                     </div>
                 </div>
-            )))}
+                <Menu
+                    id="TextMenu"
+                    open={contextMenu !== null}
+                    onClose={handleClose}
+                    anchorReference="anchorPosition"
+                    anchorPosition={
+                        contextMenu !== null ?
+                        {top: contextMenu!.mouseY, left: contextMenu!.mouseX} :
+                        undefined
+                    }
+                    >
+                    <MenuItem onClick={handleClose}> {/*TODO: Copy to clipboard*/}
+                        <div className="MenuItemWithIcon">
+                            <AttachFileIcon/>
+                            {"Copy Text"}
+                        </div>                                            
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}> 
+                        <div className="MenuItemWithIcon">
+                            <PersonOutlinedIcon/>
+                            {"Author Profile"}
+                        </div>                                            
+                    </MenuItem>                    
+                </Menu>
+                </>
+            ))):(<></>)} {/* Bruh */}
+            {messages !== undefined && messages!.length === 0 && (
+                <div className={colorTheme  === "dark" ? "messageParent darkmessage" : "messageParent lightmessage"} >
+                    <div className={"message"}> 
+                        <div className="message-text">
+                            {"This looks Empty, Be the first to send a message!"}
+                        </div>
+                    </div>
+                </div>
+                
+            
+            )}
             {/* <div className="message">
                 <div className="message-text">
                     {messages && messages.content}
@@ -155,7 +323,7 @@ export default function ChatWindow(props: IProps){
                 }} >
                 {chatHeaderContainer}
                 <Divider style={{gridRow: 2}} />                
-                {messages?.length !== 0 && conversation  /* this is where the chat messages go */}
+                {conversation  /* this is where the chat messages go */}
                 {SendForm /*this is the send input*/}
                 
             </Card>
