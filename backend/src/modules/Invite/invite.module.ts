fe40af40dbc0a474ec232843642d17ebc2eaf65d
Stijn van der Kolk <stijn@kolkies.dev>
2022-01-09 12:40:59 +0000

    hotfix: a merge deleted this

--- backend/src/modules/Invite/invite.module.ts
+++ backend/src/modules/Invite/invite.module.ts
@@ -2,10 +2,11 @@ import { PaginationQueryBuilder } from '@lib/pagination/pagination.queryBuilder'
 import { Module } from '@nestjs/common';
 import { InviteService } from './invite.service';
 import { InviteController } from './invite.controller';
+import { UserModule } from '@modules/User/user.module';
 
 @Module({
   controllers: [InviteController],
-  imports: [],
+  imports: [UserModule],
   providers: [InviteService, PaginationQueryBuilder],
 })
 export class InviteModule {}
