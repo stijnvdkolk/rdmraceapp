2d3ce859f5e20911213cc29e5bccfb1a8880a42c
Stijn van der Kolk <stijn@kolkies.dev>
2022-01-11 10:25:51 +0000

    fix: routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,29 +1,31 @@
-import { Route, Switch } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SignUp";
-import Chat from "./pages/Chat/Chat";
-import Debug from "./pages/Debug/Debug";
-import Admin from "./pages/Admin/admin";
-import Invalid from "./pages/Invalid/invalid";
-import IProps from "./components/IProps";
-import HomePage from "./pages/Home/HomePage";
-import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
-import TermsOfService from "./pages/TermsOfService/TermsOfService";
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Chat from './pages/Chat/Chat';
+import Debug from './pages/Debug/Debug';
+import Admin from './pages/Admin/admin';
+import Invalid from './pages/Invalid/invalid';
+import IProps from './components/IProps';
+import HomePage from './pages/Home/HomePage';
+import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService/TermsOfService';
 
 export default function Routes(props: IProps) {
   return (
     <Switch>
       <Route path="/Login" component={Login} />
       <Route path="/SignUp" component={SignUp} />
-      <Route path="/Homepage" component={HomePage} />
+      <Route path="/" component={HomePage} />
       <Route path="/Privacy" component={PrivacyPolicy} />
-      <Route path="/terms" component={TermsOfService} />
+      <Route path="/Terms" component={TermsOfService} />
+      <Route path="/TermsOfService" component={TermsOfService} />
+      <Route path="/Tos" component={TermsOfService} />
       <Route path="/Chat/:channelID" component={Chat} />
       <Route path="/Chat/" component={Chat} />
       <Route path="/Invalid/" component={Invalid} />
       <Route path="/Admin/:PersonID" component={Admin} />
       <Route path="/Admin/" component={Admin} />
-      <Route path="/" component={Debug} />
+      <Route path="/Debug" component={Debug} />
     </Switch>
   );
 }
