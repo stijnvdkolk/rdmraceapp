b638a44ee87177b1dba3dc7233fef232ba1519cf
Hogo <64896329+HugoVG@users.noreply.github.com>
2022-01-12 10:00:46 +0100

    Idfc please geen files aanpassen waar je niks heb gedaan ;-;

--- frontend/src/components/ChatWindow/ChatWindow.tsx
+++ frontend/src/components/ChatWindow/ChatWindow.tsx
@@ -7,9 +7,9 @@ import Message from "../../classes/Message";
 //import { useParams } from "react-router-dom";
 import IProps from "../IProps";
 import { ThemeContext } from "../theme-context";
-import EmojiEmotionsOutlinedIcon from "@mui/icons-material/EmojiEmotionsOutlined";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import "./ChatWindow.css";
 import { ConsumeEffect } from "../../API/ApiCalls";
 import { useHistory, useParams } from "react-router-dom";
@@ -20,18 +20,17 @@ import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined
 import SendIcon from '@mui/icons-material/Send';
 import {sendData} from "../../API/ApiCalls";
 
+
 function FkDateTime(date: Date) {
-  var dtm: Date = new Date(date);
-  var retVal: string = `${dtm.getDate()}/${
-    dtm.getMonth() + 1
-  }/${dtm.getFullYear()} ${dtm.getHours()}:${dtm.getMinutes()}:${dtm.getSeconds()}`;
-  return retVal;
+    var dtm: Date	= new Date(date);
+    var retVal: string = `${dtm.getDate()}/${dtm.getMonth() + 1}/${dtm.getFullYear()} ${dtm.getHours()}:${dtm.getMinutes()}:${dtm.getSeconds()}`;
+    return retVal;
 }
 function StartChat(){
 
 }
 interface UParams {
-  channelID: string | undefined;
+    channelID: string | undefined;
 }
 
 export default function ChatWindow(props: IProps){
@@ -133,36 +132,17 @@ export default function ChatWindow(props: IProps){
             
         };
 
-  const [LastClicked, setLastCLicked] = useState<string | undefined>(undefined);
-  const [ImageContext, setImageContext] = react.useState<{
-    mouseX: number;
-    mouseY: number;
-  } | null>(null);
+        const CloseProfilePictureContext = () => {
+            SetProfilePictureContext(null);
+        };
+        //#region Context Menu Functions
 
-  const handleImageContext = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setlastclickedImage(event.target);
-    //console.log((event.target as HTMLImageElement).src === undefined ? ((event.target as HTMLDivElement).firstChild as HTMLImageElement).src === undefined ? "" : (((event.target as HTMLDivElement).firstChild as HTMLImageElement).src)   : (event.target as HTMLImageElement).src);
-    //typeof(event.target) == HTMLDivElement ? setLastCLicked(event.target as HTMLDivElement) : setLastCLicked(undefined);
-    setImageContext(
-      ImageContext === null
-        ? {
-            mouseX: event.clientX - 2,
-            mouseY: event.clientY - 4,
-          }
-        : null
-    );
-  };
-  function setlastclickedImage(e: EventTarget) {
-    if ((e as HTMLImageElement).src === undefined) {
-      if ((e as HTMLDivElement).firstChild !== null) {
-        if (
-          ((e as HTMLDivElement).firstChild as HTMLImageElement).src !==
-          undefined
-        ) {
-          setLastCLicked(
-            ((e as HTMLDivElement).firstChild as HTMLImageElement).src
-          );
+        function toDataURL(url : string) {
+            return fetch(url).then((response) => {
+                    return response.blob();
+                }).then(blob => {
+                    return URL.createObjectURL(blob);
+                });
         }
         // function ToBlob(url : string) {
         //     return fetch(url).then((response) => {
@@ -239,33 +219,27 @@ export default function ChatWindow(props: IProps){
                 findLastPersonClicked(temp.id);
             }
         }
-  //TODO: REFACTOR THIS CODE
-  //TODO: Voeg PFP Click
-  const [ProfilePictureContext, SetProfilePictureContext] = react.useState<{
-    mouseX: number;
-    mouseY: number;
-  } | null>(null);
 
-  const handleProfilePictureContext = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setlastclickedImage(event.target);
-    SetProfilePictureContext(
-      ProfilePictureContext === null
-        ? {
-            mouseX: event.clientX - 2,
-            mouseY: event.clientY - 4,
-          }
-        : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
-          // Other native context menus might behave different.
-          // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
-          null
-    );
-  };
+        async function download() {
+            const a = document.createElement("a");
+            a.href = await toDataURL(LastClicked!);
+            a.download = "image.png";
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
 
-  const CloseProfilePictureContext = () => {
-    SetProfilePictureContext(null);
-  };
-  //#region Context Menu Functions
+        const downloadImage = (close: Function) =>{
+            LastClicked !== undefined ? download() : fill();
+            close();
+        }
+        //Filler test Functie
+        function fill(){
+            //empty is voor als er geen Image in buffer staat
+        }
+        //#endregion
+
+
         //#endregion
      //#region MessageLoader
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -468,25 +442,10 @@ export default function ChatWindow(props: IProps){
                 <img className="Iprofile profile" width="75" alt="" src={imageLink}/>
                 <label className="Iproduct productChat">{name}</label>
             </div>
-          );
-        })
-      ) : (
-        <></>
-      )}{" "}
-      {/* Bruh */}
-      {messages !== undefined && messages!.length === 0 && (
-        <div
-          className={
-            colorTheme === "dark"
-              ? "messageParent darkmessage"
-              : "messageParent lightmessage"
-          }
-        >
-          <div className={"message"}>
-            <div className="message-text">
-              {"This looks Empty, Be the first to send a message!"}
+            
+            <div className="channelTrailing static">
+
             </div>
-          </div>
         </div>
     );
     //#endregion
