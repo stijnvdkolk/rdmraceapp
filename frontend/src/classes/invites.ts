844fcd41afe64019c0b14d8d03f6866736bb56ad
Hogo <64896329+HugoVG@users.noreply.github.com>
2022-01-17 18:24:07 +0100

    invite to the OF

--- frontend/src/classes/invites.ts
+++ frontend/src/classes/invites.ts
@@ -1,7 +1,12 @@
 export default class Invite{
+    id!: string;
+    code!: string;
     maxUses!: number;
+    createdAt!: Date;
+    updatedAt!: Date;
+    amountUsed!: number;
+    expireAt!: Date;
     role!: "ADMIN" | "TEAM_MEMBER" | "SPONSOR" | "MARKETING";
-    expiresAt!: Date | "" ;
 }
 export class InviteBuilder{
     maxUses!: number;
