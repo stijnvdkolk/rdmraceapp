dbe7ff58dad25ea66c529e47efb9028220a04673
DavidGiesenbot <1015843@hr.nl>
2022-01-18 12:09:25 +0100

    createInvite

--- frontend/src/classes/invites.ts
+++ frontend/src/classes/invites.ts
@@ -1,15 +1,15 @@
-export default class Invite{
-    id!: string;
-    code!: string;
-    maxUses!: number;
-    createdAt!: Date;
-    updatedAt!: Date;
-    amountUsed!: number;
-    expireAt!: Date;
-    role!: "ADMIN" | "TEAM_MEMBER" | "SPONSOR" | "MARKETING";
+export default class Invite {
+  id!: string;
+  code!: string;
+  maxUses!: number;
+  createdAt!: Date;
+  updatedAt!: Date;
+  amountUsed!: number;
+  expireAt!: string;
+  role!: "ADMIN" | "TEAM_MEMBER" | "SPONSOR" | "MARKETING";
 }
-export class InviteBuilder{
-    maxUses!: number;
-    role!: "ADMIN" | "TEAM_MEMBER" | "SPONSOR" | "MARKETING";
-    expiresAt!: Date | "" ;
+export class InviteBuilder {
+  maxUses!: number;
+  role!: "ADMIN" | "TEAM_MEMBER" | "SPONSOR" | "MARKETING";
+  expiresAt!: Date | "";
 }
---
+++ frontend/src/components/CreateInvite/CreateInvite.tsx
@@ -0,0 +1,116 @@
+import { Button, Card, MenuItem, TextField } from "@mui/material";
+import React from "react";
+import { createInvite } from "../../API/Chat";
+import { InviteBuilder } from "../../classes/invites";
+
+export default function CreateInvite() {
+  const [currentSelectOption, setCurrentSelectOption] =
+    React.useState("TEAM_MEMBER");
+  const handleSelectOption = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentSelectOption(event.target.value);
+  };
+
+  const [currentMaxUses, setCurrentMaxUses] = React.useState(1);
+  const [currentDate, setCurrentDate] = React.useState<string>();
+
+  const selectOptions = [
+    {
+      value: "TEAM_MEMBER",
+      label: "Team Member",
+    },
+    {
+      value: "SPONSOR",
+      label: "Sponsor",
+    },
+    {
+      value: "MARKETING",
+      label: "Marketing",
+    },
+    {
+      value: "ADMIN",
+      label: "Admin",
+    },
+  ];
+
+  function sendRequest() {
+    console.log(currentSelectOption);
+    console.log(currentMaxUses);
+    console.log(currentDate);
+
+    if (currentDate && currentMaxUses && currentSelectOption) {
+      var invitebuilder = new InviteBuilder{
+        maxUses: (Number)currentMaxUses
+        role: ("ADMIN" | "TEAM_MEMBER" | "SPONSOR" | "MARKETING")currentSelectOption
+        expiresAt: (Date)currentDate
+      };
+      
+      var i = createInvite(invitebuilder);
+      
+    }
+  }
+
+  return (
+    <Card
+      sx={{
+        width: "400px",
+        height: "400px",
+      }}
+    >
+      <TextField
+        id="datetime-local"
+        label="Expiry date"
+        type="datetime-local"
+        sx={{ width: "250px", marginTop: "40px" }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        value={currentDate}
+        onChange={(event) => setCurrentDate(event.target.value)}
+      />
+      <br />
+      <TextField
+        type="number"
+        label="Max uses"
+        error={currentMaxUses < 1}
+        helperText={currentMaxUses < 1 ? "Min. 1" : ""}
+        sx={{
+          width: "250px",
+          marginTop: "20px",
+        }}
+        value={currentMaxUses}
+        onChange={(event) => {
+          const maxUses =
+            parseInt(event.target.value) < 1 ? 1 : parseInt(event.target.value); // niet kleiner dan 1 :D
+          setCurrentMaxUses(maxUses);
+        }}
+      />
+      <br />
+      <>
+        <TextField
+          select
+          label="Role"
+          value={currentSelectOption}
+          onChange={handleSelectOption}
+          sx={{
+            width: "250px",
+            marginTop: "20px",
+            textAlign: "left",
+          }}
+        >
+          {selectOptions.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </>
+      <br />
+      <Button
+        variant="contained"
+        sx={{ borderRadius: "16px", width: "100px", marginTop: "20px" }}
+      >
+        Create
+      </Button>
+    </Card>
+  );
+}
